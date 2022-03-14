fefbe7100bcf1bd50c50a2ab139cc4ffbc0ced4f
toniking11 <www.toni4242@mail.ru>
2022-03-14 12:36:18 +0300

    init3

--- src/com/company/lesson12/homeWork/Е.java
+++ src/com/company/lesson12/homeWork/Е.java
@@ -8,23 +8,33 @@ public class Е {
 
         int[] arr = new int[]{1, 2, 3, 3, 4, 5, 7, 5, 6, 2, 4, 7, 6};
 
+        int count = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//
+//            for (int j = i+1; j < arr.length; j++) {
+//                if (arr[i] != arr[j]) {
+//                    System.out.println(arr[i]);
+//                    break;
+//                }
+//                else break;
+//
+//            }
+//            break;
+//            }
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int a = 1; a < arr.length; a++) {
 
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[i] != arr[j]) {
-                    System.out.println(arr[i]);
-                    break;
+                if (arr[i] == arr[a]) {
+                    count++;
                 }
-                else break;
-
             }
-            break;
-
-
-
-
+            if (count == 0) {
+                System.out.println(arr[i]);
+                break;
             }
+            count = 0;
+        }
 
 
         }
