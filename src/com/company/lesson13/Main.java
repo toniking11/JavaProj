60f72164b57bda3b0e133063322c5313540cb57c
toniking11 <www.toni4242@mail.ru>
2022-03-22 21:33:49 +0300

    init4

---
+++ src/com/company/lesson13/Main.java
@@ -0,0 +1,31 @@
+package com.company.lesson13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(reader.readLine());
+        }
+
+
+        Arrays.sort(arr);
+
+        for (int i = 4; i >= 0; i--) {
+
+
+            System.out.println(arr[i]);
+
+        }
+
+    }
+}
